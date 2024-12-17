e062933134a2f70cf8fc8ce7c55319c9c5cb7d4b
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-17 11:26:13 +0200

    added annotation for repository

--- src/main/java/com/cryptoWatchlist/watchlist_service/repository/WatchlistRepository.java
+++ src/main/java/com/cryptoWatchlist/watchlist_service/repository/WatchlistRepository.java
@@ -2,6 +2,8 @@ package com.cryptoWatchlist.watchlist_service.repository;
 
 import com.cryptoWatchlist.watchlist_service.entity.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
 }
