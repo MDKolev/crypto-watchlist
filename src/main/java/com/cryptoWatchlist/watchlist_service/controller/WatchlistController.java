99ed7fdb4a84435024e4458827a48ae15f94cf0e
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-17 16:04:36 +0200

    created watchlist controller and post mapping for createWatchlist

--- src/main/java/com/cryptoWatchlist/watchlist_service/controller/WatchlistController.java
+++ src/main/java/com/cryptoWatchlist/watchlist_service/controller/WatchlistController.java
@@ -1,2 +1,27 @@
-package com.cryptoWatchlist.watchlist_service.controller;public class WatchlistController {
+package com.cryptoWatchlist.watchlist_service.controller;
+
+import com.cryptoWatchlist.watchlist_service.entity.Watchlist;
+import com.cryptoWatchlist.watchlist_service.service.WatchlistService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/watchlists")
+public class WatchlistController {
+
+    private final WatchlistService watchlistService;
+
+    public WatchlistController(WatchlistService watchlistService) {
+        this.watchlistService = watchlistService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Watchlist> createWatchlist(@RequestBody Watchlist watchlist) {
+        Watchlist savedWatchlist = watchlistService.createWatchlist(watchlist);
+        return new ResponseEntity<>(savedWatchlist, HttpStatus.CREATED);
+    }
 }
