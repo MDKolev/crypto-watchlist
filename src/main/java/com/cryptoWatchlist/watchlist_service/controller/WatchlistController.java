a532750b0334db30ef514bcdf610302c134a5b06
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-18 12:35:23 +0200

    creating a GET mapping for a watchlist by id

--- src/main/java/com/cryptoWatchlist/watchlist_service/controller/WatchlistController.java
+++ src/main/java/com/cryptoWatchlist/watchlist_service/controller/WatchlistController.java
@@ -2,11 +2,13 @@ package com.cryptoWatchlist.watchlist_service.controller;
 
 import com.cryptoWatchlist.watchlist_service.entity.Watchlist;
 import com.cryptoWatchlist.watchlist_service.service.WatchlistService;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/watchlists")
@@ -29,4 +31,11 @@ public class WatchlistController {
         List<Watchlist> allWatchlists = watchlistService.getAllWatchlists();
         return new ResponseEntity<>(allWatchlists, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Watchlist> getWatchlistById(@PathVariable long id) {
+        Watchlist watchlist = watchlistService.getWatchlistById(id);
+        return new ResponseEntity<>(watchlist, HttpStatus.OK);
+    }
+
 }
