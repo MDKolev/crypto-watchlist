8ba02b0555adfd66fd6e5fc423235d46c6387c00
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-19 11:55:28 +0200

    creating a coin service

---
+++ coin_service/src/test/java/com/coin_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
