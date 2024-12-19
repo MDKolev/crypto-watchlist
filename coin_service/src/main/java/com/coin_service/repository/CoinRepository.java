09930e4b22673089b2d701dc9c3fdbb99c3e26f1
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-19 12:12:20 +0200

    created a coin repository

---
+++ coin_service/src/main/java/com/coin_service/repository/CoinRepository.java
@@ -0,0 +1,9 @@
+package com.coin_service.repository;
+
+import com.coin_service.entity.Coin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoinRepository extends JpaRepository<Coin, Long> {
+}
