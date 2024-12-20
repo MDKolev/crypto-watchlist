d1d55e87b1c2d40f34436d71acb4abcec5ecb717
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-20 11:50:39 +0200

    created getCoinById() method

--- coin_service/src/main/java/com/coin_service/service/CoinService.java
+++ coin_service/src/main/java/com/coin_service/service/CoinService.java
@@ -2,8 +2,11 @@ package com.coin_service.service;
 
 import com.coin_service.entity.Coin;
 import com.coin_service.mapper.ManualMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -37,6 +40,19 @@ public class CoinService {
                 .filter(obj -> obj instanceof Map)
                 .map(manualMapper::mapObjectToCoin)
                 .collect(Collectors.toList());
+    }
+
+    public ResponseEntity<Coin> getCoinById(String id) {
+        Coin coin = getAllCoins().stream()
+                .filter(c -> c.getApi_id().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        if (coin != null) {
+            return ResponseEntity.ok(coin);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
