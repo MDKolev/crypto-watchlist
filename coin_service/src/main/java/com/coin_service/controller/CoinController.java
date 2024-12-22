1d2f41d331da48f322c0b4f904e4c9e60a756c15
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-22 11:44:20 +0200

    changed the GET mapping for retrieving all coins

--- coin_service/src/main/java/com/coin_service/controller/CoinController.java
+++ coin_service/src/main/java/com/coin_service/controller/CoinController.java
@@ -2,17 +2,12 @@ package com.coin_service.controller;
 
 import com.coin_service.entity.Coin;
 import com.coin_service.service.CoinService;
-import org.apache.coyote.Response;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cryptos")
+@RequestMapping("/api/coins")
 public class CoinController {
 
     private final CoinService coinService;
@@ -26,10 +21,6 @@ public class CoinController {
         return coinService.getAllCoins();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Coin> getCoinByID(@PathVariable("id") String id) {
-        return coinService.getCoinById(id);
-    }
 
 
 }
