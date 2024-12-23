c90f595c0a2dc34f8eb0fc8f4ff42a7e8e77e4ca
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-23 13:49:34 +0200

    created RestController for Alerts and a createAlert()

--- alert_service/src/main/java/com/alert_service/controller/AlertController.java
+++ alert_service/src/main/java/com/alert_service/controller/AlertController.java
@@ -1,2 +1,27 @@
-package com.alert_service.controller;public class AlertController {
+package com.alert_service.controller;
+
+import com.alert_service.entity.Alert;
+import com.alert_service.service.AlertService;
+import com.alert_service.service.AlertServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/alert")
+public class AlertController {
+
+    private final AlertServiceImpl alertService;
+
+    public AlertController(AlertServiceImpl alertService) {
+        this.alertService = alertService;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Alert> createAlert(@RequestBody Alert alert) {
+        Alert newAlert = alertService.createAlert(alert.getUserId(),
+                alert.getCoinName(), alert.getThresholdPrice());
+        return new ResponseEntity<>(newAlert, HttpStatus.CREATED);
+    }
 }
