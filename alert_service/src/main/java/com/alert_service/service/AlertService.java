c63a43e053401b78cd9429155068262556a65ab2
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-23 14:01:10 +0200

    created alert methods in interface

--- alert_service/src/main/java/com/alert_service/service/AlertService.java
+++ alert_service/src/main/java/com/alert_service/service/AlertService.java
@@ -1,8 +1,15 @@
 package com.alert_service.service;
 
+import com.alert_service.entity.Alert;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface AlertService {
+
+    Alert createAlert(Long userId, String coinName, Double thresholdPRice);
+
+    List<Alert> getAllAlerts();
 
 
 }
