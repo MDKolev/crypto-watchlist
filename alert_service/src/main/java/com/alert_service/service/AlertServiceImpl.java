c897813163a734f7a28bab7b3310c978d0654d67
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-23 13:47:59 +0200

    create alert service implementation and createAlert method

--- alert_service/src/main/java/com/alert_service/service/AlertServiceImpl.java
+++ alert_service/src/main/java/com/alert_service/service/AlertServiceImpl.java
@@ -1,2 +1,32 @@
-package com.alert_service.service;public class AlertServiceImpl {
+package com.alert_service.service;
+
+import com.alert_service.entity.Alert;
+import com.alert_service.repository.AlertRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+
+@Service
+public class AlertServiceImpl implements AlertService{
+
+    private final AlertRepository alertRepository;
+
+    public AlertServiceImpl(AlertRepository alertRepository) {
+        this.alertRepository = alertRepository;
+    }
+
+    public Alert createAlert(Long userId, String coinName, Double thresholdPRice) {
+        Alert newAlert = new Alert();
+        newAlert.setUserId(userId);
+        newAlert.setCoinName(coinName);
+        newAlert.setThresholdPrice(thresholdPRice);
+        newAlert.setTriggered(false);
+        newAlert.setCreatedAt(Instant.now());
+
+        return alertRepository.save(newAlert);
+    }
+
 }
