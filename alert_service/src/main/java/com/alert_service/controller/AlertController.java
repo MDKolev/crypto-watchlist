5a302170c524bc979946d71a05e7060aee14f136
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-23 17:40:00 +0200

    changed POST request for creating an alert

--- alert_service/src/main/java/com/alert_service/controller/AlertController.java
+++ alert_service/src/main/java/com/alert_service/controller/AlertController.java
@@ -3,10 +3,13 @@ package com.alert_service.controller;
 import com.alert_service.entity.Alert;
 import com.alert_service.service.AlertService;
 import com.alert_service.service.AlertServiceImpl;
+import com.coin_service.entity.Coin;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -21,10 +24,10 @@ public class AlertController {
     }
 
     @PostMapping("/new")
-    public ResponseEntity<Alert> createAlert(@RequestBody Alert alert) {
-        Alert newAlert = alertService.createAlert(alert.getUserId(),
-                alert.getCoinName(), alert.getThresholdPrice());
-        return new ResponseEntity<>(newAlert, HttpStatus.CREATED);
+    public ResponseEntity<Alert> createAlert(@RequestBody Alert newAlert) {
+        Alert alert = alertService.createAlert(newAlert);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(alert);
     }
 
     @GetMapping("/all")
