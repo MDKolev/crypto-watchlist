8c3fe13a74b90e912280c14caf01f3effe4f1b1a
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-23 13:01:15 +0200

    created an alert repository

--- alert_service/src/main/java/com/alert_service/repository/AlertRepository.java
+++ alert_service/src/main/java/com/alert_service/repository/AlertRepository.java
@@ -1,2 +1,9 @@
-package com.alert_service.repository;public class AlertRepository {
+package com.alert_service.repository;
+
+import com.alert_service.entity.Alert;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
 }
