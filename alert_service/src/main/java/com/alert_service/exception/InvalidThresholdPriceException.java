c30503b43714a2825ebea59a6dac7f1f5be18fde
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-24 11:29:23 +0200

    created an exception for handling an invalid threshold price

--- alert_service/src/main/java/com/alert_service/exception/InvalidThresholdPriceException.java
+++ alert_service/src/main/java/com/alert_service/exception/InvalidThresholdPriceException.java
@@ -1,2 +1,8 @@
-package com.alert_service.exception;public class InvalidThresholdPriceException {
+package com.alert_service.exception;
+
+public class InvalidThresholdPriceException extends RuntimeException{
+
+    public InvalidThresholdPriceException(String message) {
+        super(message);
+    }
 }
