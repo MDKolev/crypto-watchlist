c43a9b14036a335b4cd96748a1a5dc4b56c07d46
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-24 12:15:12 +0200

    created a global exception handler

--- alert_service/src/main/java/com/alert_service/exception/GlobalExceptionHandler.java
+++ alert_service/src/main/java/com/alert_service/exception/GlobalExceptionHandler.java
@@ -1,2 +1,26 @@
-package com.alert_service.exception;public class GlobalExceptionHandler {
+package com.alert_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AlertNotFoundException.class)
+    public ResponseEntity<String> handleAlertNotFoundException(AlertNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InvalidThresholdPriceException.class)
+    public ResponseEntity<String> handleInvalidThresholdPriceException(InvalidThresholdPriceException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(CoinNotFoundException.class)
+    public ResponseEntity<String> handleCoinNotFoundException(CoinNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
 }
