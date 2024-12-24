da88af5610aa2a0aa8da1b2fb6e40132517a419b
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-24 18:14:06 +0200

    created a global exception handler

--- coin_service/src/main/java/com/coin_service/exception/GlobalExceptionHandler.java
+++ coin_service/src/main/java/com/coin_service/exception/GlobalExceptionHandler.java
@@ -1,2 +1,15 @@
-package com.coin_service.exception;public class GlobalExceptionHandler {
+package com.coin_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoCoinsToSaveException.class)
+    public ResponseEntity<String> handleNoCoinsToSaveException(NoCoinsToSaveException exception ) {
+        return ResponseEntity.status(HttpStatus.NOT_MODIFIED).body(exception.getMessage());
+    }
 }
