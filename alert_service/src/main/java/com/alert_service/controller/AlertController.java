5b7fca65fcddbee9ab2b8a475421e8d43fed8a16
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-24 12:17:56 +0200

    refractored the controller

--- alert_service/src/main/java/com/alert_service/controller/AlertController.java
+++ alert_service/src/main/java/com/alert_service/controller/AlertController.java
@@ -2,16 +2,12 @@ package com.alert_service.controller;
 
 import com.alert_service.entity.Alert;
 import com.alert_service.entity.UpdateThresholdPriceDTO;
-import com.alert_service.exception.AlertNotFoundException;
-import com.alert_service.exception.InvalidThresholdPriceException;
 import com.alert_service.service.AlertServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/alert")
@@ -26,7 +22,6 @@ public class AlertController {
     @PostMapping("/new")
     public ResponseEntity<Alert> createAlert(@RequestBody Alert newAlert) {
         Alert alert = alertService.createAlert(newAlert);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(alert);
     }
 
@@ -37,33 +32,19 @@ public class AlertController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getAlertById(@PathVariable Long id) {
-        try {
         Alert alert = alertService.getAlertById(id);
         return new ResponseEntity<>(alert, HttpStatus.FOUND);
-        } catch (AlertNotFoundException ex) {
-            Map<String, String> errorResponse = new HashMap<>();
-            errorResponse.put("message", ex.getLocalizedMessage());
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
     }
 
     @PutMapping("/{id}/update")
-    public ResponseEntity<?> updateThresholdPrice(@PathVariable Long id, @RequestBody UpdateThresholdPriceDTO dto) {
-        try {
+    public ResponseEntity<Alert> updateThresholdPrice(@PathVariable Long id, @RequestBody UpdateThresholdPriceDTO dto) {
             Alert alert = alertService.updateThresholdPrice(id, dto.getThresholdPrice());
             return new ResponseEntity<>(alert, HttpStatus.ACCEPTED);
-        } catch (InvalidThresholdPriceException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(ex.getLocalizedMessage());
-        }
     }
 
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<String> deleteAlertById(@PathVariable Long id){
-        try {
             alertService.deleteAlert(id);
             return ResponseEntity.ok("Alert with ID " + id + " was successfully deleted!");
-        } catch (AlertNotFoundException ex) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getLocalizedMessage());
-        }
     }
 }
