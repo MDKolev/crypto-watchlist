c47bc8fcda37b66dee5b8dbeb31d58a0e27abacd
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-24 17:46:01 +0200

    refratored to use WebClient instead of RestTemplate

--- coin_service/src/main/java/com/coin_service/configuration/AppConfig.java
+++ coin_service/src/main/java/com/coin_service/configuration/AppConfig.java
@@ -3,14 +3,14 @@ package com.coin_service.configuration;
 import com.coin_service.mapper.ManualMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://api.coingecko.com/api/v3").build();
     }
 
     @Bean
