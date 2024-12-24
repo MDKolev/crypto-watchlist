4989416da0d898d07b703a524b16358b752c200d
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-24 12:17:33 +0200

    refractored the service in accordance with the exception

--- alert_service/src/main/java/com/alert_service/service/AlertServiceImpl.java
+++ alert_service/src/main/java/com/alert_service/service/AlertServiceImpl.java
@@ -2,6 +2,7 @@ package com.alert_service.service;
 
 import com.alert_service.entity.Alert;
 import com.alert_service.exception.AlertNotFoundException;
+import com.alert_service.exception.CoinNotFoundException;
 import com.alert_service.exception.InvalidThresholdPriceException;
 import com.alert_service.repository.AlertRepository;
 import com.coin_service.entity.Coin;
@@ -30,18 +31,19 @@ public class AlertServiceImpl implements AlertService{
                 .bodyToMono(Coin.class);
     }
 
-    //todo: take care of NullPointerException at line35
     public Alert createAlert(Alert alert) {
         Coin coin = fetchCoinById(alert.getCoinId()).block();
 
-        Alert newAlert = new Alert();
-        newAlert.setCoinId(coin.getId());
-        newAlert.setUserId(null);
-        newAlert.setThresholdPrice(alert.getThresholdPrice());
-        newAlert.setCreatedAt(Instant.now());
-
-        return alertRepository.save(newAlert);
-
+        if (coin != null) {
+            Alert newAlert = new Alert();
+            newAlert.setCoinId(coin.getId());
+            newAlert.setUserId(null);
+            newAlert.setThresholdPrice(alert.getThresholdPrice());
+            newAlert.setCreatedAt(Instant.now());
+            return alertRepository.save(newAlert);
+        } else {
+            throw new CoinNotFoundException();
+        }
     }
 
     public List<Alert> getAllAlerts() {
@@ -50,31 +52,28 @@ public class AlertServiceImpl implements AlertService{
 
     public Alert getAlertById(Long id) {
         Optional<Alert> alert = alertRepository.findById(id);
+
         if (alert.isPresent()) {
             return alert.get();
         } else {
-            throw new AlertNotFoundException("Could not find alert with ID: " + id);
+            throw new AlertNotFoundException(id);
         }
     }
 
     public Alert updateThresholdPrice(Long id, Double thresholdPrice) {
-        if(!alertRepository.existsById(id)) {
-            throw new AlertNotFoundException("Could not find alert with ID: " + id);
-        }
-
         if(thresholdPrice > 0) {
             Alert alertById = getAlertById(id);
             alertById.setThresholdPrice(thresholdPrice);
             alertRepository.save(alertById);
             return alertById;
         } else {
-            throw new InvalidThresholdPriceException("Invalid threshold price - " + thresholdPrice);
+            throw new InvalidThresholdPriceException(thresholdPrice);
         }
     }
 
     public void deleteAlert(Long id) {
         if(!alertRepository.existsById(id)) {
-            throw new AlertNotFoundException("Could not find alert with ID: " + id);
+            throw new AlertNotFoundException(id);
         }
         alertRepository.deleteById(id);
     }
