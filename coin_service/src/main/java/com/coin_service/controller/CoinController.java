83616674c071add1ec03f3cfee72cda830bbaa1f
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-25 20:11:37 +0200

    refractored coin controller

--- coin_service/src/main/java/com/coin_service/controller/CoinController.java
+++ coin_service/src/main/java/com/coin_service/controller/CoinController.java
@@ -2,12 +2,8 @@ package com.coin_service.controller;
 
 import com.coin_service.entity.Coin;
 import com.coin_service.entity.CoinDetailsForWatchlistDTO;
-<<<<<<< HEAD
 import com.coin_service.service.CoinService;
 import org.springframework.http.HttpStatus;
-=======
-import com.coin_service.service.CoinServiceImpl;
->>>>>>> alert_service
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -17,43 +13,30 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/api/coins")
 public class CoinController {
 
-    private final CoinServiceImpl coinServiceImpl;
+    private final CoinService coinService;
 
-    public CoinController(CoinServiceImpl coinServiceImpl) {
-        this.coinServiceImpl = coinServiceImpl;
+    public CoinController(CoinService coinService) {
+        this.coinService = coinService;
     }
 
     @GetMapping("/all")
-<<<<<<< HEAD
     public Flux<Coin> getAllCoins() {
         return ResponseEntity.status(HttpStatus.OK).body(coinService.getAllCoins()).getBody();
-=======
-    public List<Coin> getAllCoins() {
-        return coinServiceImpl.getAllCoins();
->>>>>>> alert_service
     }
 
     @PutMapping("/save")
     public void saveCoins() {
-        coinServiceImpl.saveCoins();
+        coinService.saveCoins();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Coin> getCoinByID(@PathVariable("id") String id) {
-<<<<<<< HEAD
         return new ResponseEntity<>(coinService.getCoinById(id), HttpStatus.FOUND);
-=======
-        return coinServiceImpl.getCoinById(id);
->>>>>>> alert_service
     }
 
     @GetMapping("/{id}/details")
     public ResponseEntity<CoinDetailsForWatchlistDTO> getCoinDetailsForWatchlistDTO(@PathVariable("id") String id) {
-<<<<<<< HEAD
         return new ResponseEntity<>(coinService.getCoinDetailsForWatchlist(id), HttpStatus.OK);
-=======
-        return coinServiceImpl.getCoinDetailsForWatchlist(id);
->>>>>>> alert_service
     }
 
 }
