18b8029043d6c66be701010fe81a6e7e41b88b58
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-26 12:48:59 +0200

    created a dto for creating a new watchlist

--- watchlist_service/src/main/java/com/watchlist_service/entity/NewWatchlistDTO.java
+++ watchlist_service/src/main/java/com/watchlist_service/entity/NewWatchlistDTO.java
@@ -1,2 +1,27 @@
-package com.watchlist_service.entity;public record NewWatchlistDTO() {
+package com.watchlist_service.entity;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class NewWatchlistDTO {
+
+    @NotBlank(message = "Watchlist name is required.")
+    private String watchlistName;
+
+    private FiatCurrency fiatCurrency;
+
+    public String getWatchlistName() {
+        return watchlistName;
+    }
+
+    public void setWatchlistName(String watchlistName) {
+        this.watchlistName = watchlistName;
+    }
+
+    public FiatCurrency getFiatCurrency() {
+        return fiatCurrency;
+    }
+
+    public void setFiatCurrency(String fiatCurrency) {
+       this.fiatCurrency = FiatCurrency.fromString(fiatCurrency);
+    }
 }
