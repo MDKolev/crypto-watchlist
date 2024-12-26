e18daac78100cdab1bf71183c1cacead39f6b6c9
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-26 12:49:46 +0200

    created a method to handle invalid fields when creating a new watchlist

--- watchlist_service/src/main/java/com/watchlist_service/exception/GlobalExceptionHandler.java
+++ watchlist_service/src/main/java/com/watchlist_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.watchlist_service.exception;
 
 
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,5 +18,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidFiatCurrencyException.class)
     public ResponseEntity<Object> handleInvalidFiatCurrencyException(InvalidFiatCurrencyException exception) {
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UnrecognizedPropertyException.class)
+    public ResponseEntity<String> handleUnrecognizedPropertyException(UnrecognizedPropertyException ex) {
+        return new ResponseEntity<>("Unrecognized field: " + ex.getPropertyName(), HttpStatus.BAD_REQUEST);
     }
 }
