15319ef2b1b18766f1d8a898d86b074f0a08b05e
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-28 12:16:37 +0200

    changed message of CoinNotFoundException

--- alert_service/src/main/java/com/alert_service/exception/CoinNotFoundException.java
+++ alert_service/src/main/java/com/alert_service/exception/CoinNotFoundException.java
@@ -2,7 +2,7 @@ package com.alert_service.exception;
 
 public class CoinNotFoundException extends RuntimeException{
 
-    public CoinNotFoundException() {
-        super("Coin with this ID does not exist!");
+    public CoinNotFoundException(String coinId) {
+        super("Could not find coin with ID: " + coinId);
     }
 }
