a1c5b6ea4e3b9a2b31816a7897c78c2d8196a63b
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-28 14:19:53 +0200

    refractored createWatchlist() to contain a check if watchlist with this name already exist

--- watchlist_service/src/main/java/com/watchlist_service/service/WatchlistServiceImpl.java
+++ watchlist_service/src/main/java/com/watchlist_service/service/WatchlistServiceImpl.java
@@ -4,16 +4,17 @@ import com.coin_service.entity.Coin;
 import com.coin_service.exception.CoinNotFoundException;
 import com.watchlist_service.entity.NewWatchlistDTO;
 import com.watchlist_service.entity.Watchlist;
+import com.watchlist_service.exception.WatchlistAlreadyExistException;
 import com.watchlist_service.exception.WatchlistContainThisCoinException;
 import com.watchlist_service.exception.WatchlistNotFoundException;
 import com.watchlist_service.repository.WatchlistRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import reactor.core.publisher.Mono;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -32,6 +33,13 @@ public class WatchlistServiceImpl implements WatchlistService {
 
     @Override
     public Watchlist createWatchlist(NewWatchlistDTO newWatchlistDTO) {
+        Watchlist checkForWatchlist = watchlistRepository.findByWatchlistName(newWatchlistDTO.getWatchlistName());
+        String watchlistName = checkForWatchlist.getWatchlistName();
+
+        if (!watchlistName.isEmpty()) {
+            throw new WatchlistAlreadyExistException(newWatchlistDTO.getWatchlistName());
+        }
+
         Watchlist watchlist = new Watchlist();
 
         watchlist.setWatchlistName(newWatchlistDTO.getWatchlistName());
