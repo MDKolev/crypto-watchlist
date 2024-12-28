50a0ea864aa28d16d8251170b9174a3ffa0e8dd0
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-28 09:58:01 +0200

    replaced WebClient with RestTemplate

--- watchlist_service/src/main/java/com/watchlist_service/configuration/AppConfig.java
+++ watchlist_service/src/main/java/com/watchlist_service/configuration/AppConfig.java
@@ -2,13 +2,15 @@ package com.watchlist_service.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8081/api/coins").build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+
     }
 }
