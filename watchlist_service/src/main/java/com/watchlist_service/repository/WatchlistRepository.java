9efa4795a000a5f34fafab0a59a5018e9633e40d
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-28 14:19:06 +0200

    created a findByWatchlistName

--- watchlist_service/src/main/java/com/watchlist_service/repository/WatchlistRepository.java
+++ watchlist_service/src/main/java/com/watchlist_service/repository/WatchlistRepository.java
@@ -3,7 +3,9 @@ package com.watchlist_service.repository;
 import com.watchlist_service.entity.Watchlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+    Watchlist findByWatchlistName(String watchlistName);
 }
