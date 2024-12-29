d95bd31469a248e6f2ef06a59a144136c01a00c1
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-29 14:50:24 +0200

    created an AuthenticationController

--- user_service/src/main/java/com/user_service/auth/AuthenticationController.java
+++ user_service/src/main/java/com/user_service/auth/AuthenticationController.java
@@ -1,2 +1,28 @@
-package com.user_service.auth;public class AuthenticationController {
+package com.user_service.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
 }
