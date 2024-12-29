78ba423a9b611fb6fbce8ec44c82ebb335892544
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-29 14:51:30 +0200

    created a demo controller for testing

--- user_service/src/main/java/com/user_service/demo/DemoController.java
+++ user_service/src/main/java/com/user_service/demo/DemoController.java
@@ -1,2 +1,16 @@
-package com.user_service.demo;public class DemoController {
+package com.user_service.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user-demo")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("hello from secured endpoint");
+    }
 }
