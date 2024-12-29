017f5b48a6cd1ec9be3f14c186e58b946c8d3c6a
MDKolev <113345588+MDKolev@users.noreply.github.com>
2024-12-29 16:14:35 +0200

    refractored userDetailsService to throw an error when email is not registered

--- user_service/src/main/java/com/user_service/configuration/AppConfig.java
+++ user_service/src/main/java/com/user_service/configuration/AppConfig.java
@@ -1,5 +1,6 @@
 package com.user_service.configuration;
 
+import com.user_service.exception.NoUserWIthThisEmailException;
 import com.user_service.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,7 +26,7 @@ public class AppConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new NoUserWIthThisEmailException(username));
     }
 
     @Bean
